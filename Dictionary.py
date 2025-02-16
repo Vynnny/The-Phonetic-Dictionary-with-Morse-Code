d8e85c1f3ed79b04d3ce2147eb41aab4ea6f4d89
Vynnny <diego.b.23456@gmail.com>
2025-02-16 09:59:11 +0800

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,32 @@
+DATA = {
+
+"A": "Translation - Alpha •-",
+"B": "Translation - Bravo -•••",
+"C": "Translation - Charlie -•-•",
+"D": "Translation - Delta -••",
+"E": "Translation - Echo •",
+"F": "Translation - Foxtrot ••-•",
+"G": "Translation - Golf --•",
+"H": "Translation - Hotel ••••",
+"I": "Translation - India ••",
+"J": "Translation - Juliet •---",
+"K": "Translation - Kilo -•-",
+"L": "Translation - Lima •-••",
+"M": "Translation - Mike --",
+"N": "Translation - November -•",
+"O": "Translation - Oscar ---",
+"P": "Translation - Papa ---",
+"Q": "Translation - Quebec --•-",
+"R": "Translation - Romeo •-•",
+"S": "Translation - Sierra •••",
+"T": "Translation - Tango -",
+"U": "Translation - Uniform ••-",
+"V": "Translation - Victor •••-",
+"W": "Translation - Whiskey •--",
+"X": "Translation - X-Ray -••-",
+"Y": "Translation - Yankee -•--",
+"Z": "Translation - Zulu --••"
+}
+
+a = input("Word:")
+print(DATA[a])
